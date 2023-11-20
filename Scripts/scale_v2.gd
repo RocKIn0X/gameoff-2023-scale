0fa233571412f346cd4ad7c5e9b2199b880aaeac
RocKIn0X <dantai39@gmail.com>
2023-11-20 23:53:38 +0700

    set clipping mask for fish meat sprite

--- Scripts/scale_v2.gd
+++ Scripts/scale_v2.gd
@@ -14,47 +14,47 @@ var hit_time: float
 var trail : Node
 
 func hit():
-    print("hit")
-    if is_fx:
-        return
-    health -= 1
-    hit_time = Time.get_unix_time_from_system()
-    if health <= 0:
-        _to_fx()
+	print("hit")
+	if is_fx:
+		return
+	health -= 1
+	hit_time = Time.get_unix_time_from_system()
+	if health <= 0:
+		_to_fx()
 
 
 func _to_fx():
-    is_fx = true
-    gravity_scale = 10.0
-    var direction = randf() - 0.5
-    var force = (randf() + 0.5) * bounce_force
-    var rand_force = Vector2(direction, -1.0 + abs(direction)) * force
-    apply_force(rand_force)
-    trail = trail_prefab.instantiate()
-    trail.tracked_object = self
-    trail.position = Vector2(0, 0)
-    trail.global_position = Vector2(0, 0)
-    $"..".add_child(trail)
+	is_fx = true
+	gravity_scale = 10.0
+	var direction = randf() - 0.5
+	var force = (randf() + 0.5) * bounce_force
+	var rand_force = Vector2(direction, -1.0 + abs(direction)) * force
+	apply_force(rand_force)
+	trail = trail_prefab.instantiate()
+	trail.tracked_object = self
+	trail.position = Vector2(0, 0)
+	trail.global_position = Vector2(0, 0)
+	$"..".add_child(trail)
 
 
 func _on_fx_done():
-    onDestroy.emit()
-    if trail != null:
-        trail.queue_free()
-    $CollisionShape2D.disabled = false;
-    queue_free()
+	onDestroy.emit()
+	if trail != null:
+		trail.queue_free()
+	$CollisionShape2D.disabled = false;
+	queue_free()
 
 
 func _on_mouse_entered():
-    print("mouse")
-    hit();
+	print("mouse")
+	hit();
 
 
 func _physics_process(_delta):
-    var current_time := Time.get_unix_time_from_system()
-    if current_time < hit_time + hit_blink_ms:
-        modulate = hit_color
-    else:
-        modulate = Color.WHITE
-    if is_fx and current_time > hit_time + bounce_ms:
-        _on_fx_done()
+	var current_time := Time.get_unix_time_from_system()
+	if current_time < hit_time + hit_blink_ms:
+		modulate = hit_color
+	else:
+		modulate = Color.WHITE
+	if is_fx and current_time > hit_time + bounce_ms:
+		_on_fx_done()
--- Scripts/trail.gd
+++ Scripts/trail.gd
@@ -5,22 +5,22 @@ var stored_points: Array
 @export var MAX_LENGTH: int
 
 func _physics_process(_delta: float) -> void:
-    if stored_points.size() > MAX_LENGTH:
-        _remove_trail_tail()
-        
-    if tracked_object != null:
-        _append_trail_head(tracked_object.global_position)
+	if stored_points.size() > MAX_LENGTH:
+		_remove_trail_tail()
+		
+	if tracked_object != null:
+		_append_trail_head(tracked_object.global_position)
 
 func _remove_trail_tail():
-    stored_points.pop_front()
-    remove_point(0)
+	stored_points.pop_front()
+	remove_point(0)
 
 
 func _append_trail_head(pos: Vector2):
-    stored_points.append(pos)
-    add_point(pos)
+	stored_points.append(pos)
+	add_point(pos)
 
 
 func _ready():
-    reparent(get_tree().root, false)
-    global_position = Vector2.ZERO
+	reparent(get_tree().root, false)
+	global_position = Vector2.ZERO
