75aeef8f5454ea5b468de4f066df7c414151576e
SpntKbrg <suphanathk@keemail.me>
2023-11-29 01:33:38 +0700

    Revert "Add remover trail # Conflicts: #	Scenes/main.tscn"
    This reverts commit a2a9d9ab37a521bde90718b2a6a7e98cf3d6c162.

--- Scripts/trail.gd
+++ Scripts/trail.gd
@@ -12,7 +12,6 @@ func _physics_process(_delta: float) -> void:
 		
 	if tracked_object != null:
 		_append_trail_head(tracked_object.global_position)
-	
 
 func _remove_trail_tail():
 	stored_points.pop_front()
@@ -27,17 +26,7 @@ func _append_trail_head(pos: Vector2):
 func _ready():
 	reparent(get_tree().root, false)
 	global_position = Vector2.ZERO
-	VarManager.on_data_changed.connect(_on_var_changed)
-	_on_fever_changed(VarManager.get_data(Varkey.B_IS_FEVER))
-
-
-func _on_var_changed(path: String, new_val, old_val):
-	if path == Varkey.B_IS_FEVER:
-		_on_fever_changed(new_val)
-
-
-func _on_fever_changed(new_fever: bool):
-	if new_fever:
+	if VarManager.get_data(Varkey.B_IS_FEVER):
 		gradient = fever_gradiant
 	else:
 		gradient = normal_gradiant
